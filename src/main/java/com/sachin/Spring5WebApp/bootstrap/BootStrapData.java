b9c55e1130f3be2b4db2780ed1fffd57415cd5c8
sachinmewar <sachinmewar800@gmail.com>
2022-02-18 22:44:31 +0530

    Added Publisher

--- src/main/java/com/sachin/Spring5WebApp/bootstrap/BootStrapData.java
+++ src/main/java/com/sachin/Spring5WebApp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.sachin.Spring5WebApp.bootstrap;
 
 import com.sachin.Spring5WebApp.domain.Author;
 import com.sachin.Spring5WebApp.domain.Book;
+import com.sachin.Spring5WebApp.domain.Publisher;
 import com.sachin.Spring5WebApp.repository.AuthorRepository;
 import com.sachin.Spring5WebApp.repository.BookRepository;
+import com.sachin.Spring5WebApp.repository.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,13 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -23,9 +28,14 @@ public class BootStrapData implements CommandLineRunner {
 
         Author sachin = new Author( "Sachin", "Mewar");
         Book book = new Book("Book", "3122342323421");
+        Publisher publisher = new Publisher("McGraw", "Srinagar", "Uttarakhand", "pauri", "246174");
+
+        publisherRepository.save(publisher);
 
         sachin.getBooks().add(book);
         book.getAuthors().add(sachin);
+        publisher.getBooks().add(book);
+        book.setPublisher(publisher);
 
         authorRepository.save(sachin);
         bookRepository.save(book);
@@ -34,6 +44,8 @@ public class BootStrapData implements CommandLineRunner {
         Book javaBook = new Book("Java Master", "2134312312321");
         honey.getBooks().add(javaBook);
         book.getAuthors().add(honey);
+        publisher.getBooks().add(javaBook);
+        javaBook.setPublisher(publisher);
 
         authorRepository.save(honey);
         bookRepository.save(javaBook);
@@ -41,5 +53,6 @@ public class BootStrapData implements CommandLineRunner {
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books : " + bookRepository.count());
         System.out.println("Number of Authors : " + authorRepository.count());
+        System.out.println("Number of Publishers: " + publisherRepository.count());
     }
 }
